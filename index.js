538cca3faacc8f8cd5c6470e44501727d339e326
tbushman <tbushman@pu.bli.sh>
2016-10-05 14:56:43 -0600

    basic useragent.express implementation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var url = require('url');
 var bodyParser = require('body-parser');
 var http = require('http');
+var useragent = require('express-useragent');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -12,60 +13,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.json());
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.use(urlencodedParser);
+app.use(useragent.express());
 
-app.get(/(.+)/, function (req, res) { //regexp checks if at least one char in path
-	var dateInput = url.parse(req.url).pathname;
-	dateInput = dateInput.replace(/%20/g, ' ').replace('/', ''); //this.. path includes slash
+
+app.get('/', function (req, res) { //regexp checks if at least one char in path
+	var userAgentInfo = req.useragent;
 	res.render('index', {
-		title: 'FCC Timestamp Microservice',
-		response: formatDate(dateInput)
+		title: 'FCC Request Header Microservice',
+		response: formatInfo(userAgentInfo)
 	});
 });
 
-app.post('/', urlencodedParser, function(req, res){ //if path empty, post from form
-	var dateInput = req.body.date;	
-	res.render('index', {
-		title: 'FCC Timestamp Microservice',
-		response: formatDate(dateInput)
-	});
-});
-
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-function formatDate(dateInput){
-	var newDate = new Date(''+dateInput+'');
+function formatInfo(userAgentInfo){
 	
-	if (newDate == NaN || newDate == "Invalid Date") {
-		var convertDate = new Date(dateInput*1000); //try converting to milliseconds
-		console.log(convertDate)
-		console.log(dateInput)
-		if (convertDate == "Invalid Date" || convertDate == NaN) {
-			response = {
-				"unix": null,
-				"natural": null
-			}	
-			return JSON.stringify(response);
-		} else {
-			var year = convertDate.getFullYear();
-			var month = months[convertDate.getMonth()];
-			var day = convertDate.getDate();
-			var unixDate = Math.floor(Date.parse(convertDate)/1000); //convert back to seconds from milliseconds
-		}
-	} else {
-		var year = newDate.getFullYear();
-		var month = months[newDate.getMonth()];
-		var day = newDate.getDate();
-		var unixDate = Math.floor(Date.parse(newDate)/1000);
-	}
-	var natural = ""+month+" "+day+", "+year+"";
-	response = {
-		"unix": unixDate,
-		"natural": natural
-	}
-	return JSON.stringify(response);
+	return JSON.stringify(userAgentInfo);
 }
 
 
