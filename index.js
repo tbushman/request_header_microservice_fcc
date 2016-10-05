c918a1bc8cb0139c5cd1593a00b7b1de938a3029
tbushman <tbushman@pu.bli.sh>
2016-10-05 17:51:36 -0600

    added language, tested ip

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var url = require('url');
 var bodyParser = require('body-parser');
 var http = require('http');
 var useragent = require('express-useragent');
+//var requestIp = require('request-ip');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -14,19 +16,41 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(useragent.express());
-
-
+//app.enable('trust proxy');
+app.set('trust proxy', true);
 app.get('/', function (req, res) { 
+	var clientIp = req.ip;
+	var language = req.headers['accept-language'].split(',')[0];
+	console.log(language)
+	//requestIp.getClientIp(req); 
+	//req.headers['X-Forwarded-For'];
+	//JSON.stringify(req.headers['X-Forwarded-For'])
+	//JSON.stringify(req.ip)
+	//JSON.stringify(req.headers['x-real-ip'])
+	//JSON.stringify(req.connection.remoteAddress);
+	//JSON.stringify(req.headers) || req.connection.remoteAddress;
+	console.log(clientIp)
+	//
 	var userAgentInfo = req.useragent;
+	var os = userAgentInfo.os;
+	var osV = userAgentInfo.version;
+	var hardware = userAgentInfo.platform;
+	var sys = ''+hardware+'; '+os+'; '+osV+'';
+	var resp = {
+		ip: clientIp,
+		lang: language,
+		sys: sys
+	}
+	
 	res.render('index', {
 		title: 'FCC Request Header Microservice',
-		response: formatInfo(userAgentInfo)
+		response: formatInfo(resp)
 	});
 });
 
-function formatInfo(userAgentInfo){
+function formatInfo(resp){
 	
-	return JSON.stringify(userAgentInfo);
+	return JSON.stringify(resp);
 }
 
 
